baa8ab239446469362373d1f533b2d94ef5255a4
Pedro Henrique Dornela Gonçalves <pedro.henriquedgoncalves@gmail.com>
2024-09-22 13:08:18 -0300

    Primeiro Commit. Extraindo o valor de tags específicas.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        //Local file
+        var caminhoUsuarioLocal = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        string nomeArquivo = "";//Nome do arquivo mais .xml vai aqui
+        var caminhoArquivo = $"{caminhoUsuarioLocal}\\Downloads\\{nomeArquivo}";
+
+        XmlDocument xmlDoc = new();
+        xmlDoc.Load(caminhoArquivo);
+
+        XmlNodeList nomeProd = xmlDoc.GetElementsByTagName("xProd");
+        XmlNodeList valorProd = xmlDoc.GetElementsByTagName("vProd");
+        XmlNodeList valorICMS = xmlDoc.GetElementsByTagName("vICMS");
+        XmlNodeList valorIPI = xmlDoc.GetElementsByTagName("vIPI");
+
+        string tagName = "prod";
+        int count = xmlDoc.GetElementsByTagName(tagName).Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"{nomeProd[i]?.InnerText} -> {valorProd[i]?.InnerText}");
+        }
+
+        Console.WriteLine($@"Valor ICMS: {valorICMS[0]?.InnerText}");
+        Console.WriteLine($"Valor IPI: {valorIPI[0]?.InnerText}");
+
+
+    }
+
+}
